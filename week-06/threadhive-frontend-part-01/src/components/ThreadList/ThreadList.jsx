baa69626fb3eb6c901745381ce0957b86790ac84
Prashanth Puranik <puranik@digdeeper.in>
2026-07-18 20:47:22 +0530

    simple list rendering

--- week-06/threadhive-frontend-part-01/src/components/ThreadList/ThreadList.jsx
+++ week-06/threadhive-frontend-part-01/src/components/ThreadList/ThreadList.jsx
@@ -5,13 +5,19 @@ export default function ThreadList({ threads, onSelect }) {
   const handleUpvote = () => {
     alert('Upvote clicked!');
   };
+
   const handleDownvote = () => {
     alert('Downvote clicked!');
   };
 
+  // now we have an array of divs
+  const els = threads.map(
+    t => <div>{t.title}</div>
+  )
+
   return (
     <Container fluid className="px-0">
-      Hello from ThreadList
+      {els}
     </Container>
   );
 }
